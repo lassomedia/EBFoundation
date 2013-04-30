8fc12aea7642eea737d637c996676eea6f2ac2df
Dave Keck <dave@getshacked.com>
2013-04-29 16:28:58 -1000

    Removing EBObservation (moving it to its own repo)

--- Code/EBFoundation.h
+++ Code/EBFoundation.h
@@ -1,5 +1,4 @@
 #import <EBFoundation/EBAssert.h>
-#import <EBFoundation/EBObservation.h>
 #import <EBFoundation/EBSingleton.h>
 #import <EBFoundation/EBTime.h>
 #import <EBFoundation/EBUtilities.h>
